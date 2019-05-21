8fa9f51e6fcfd07f88db5e26d2ecf2136875f5a1
Takuya Iida <takuya28@gmail.com>
2019-05-21 23:22:23 +0900

    fix: nuxt.render processing

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,6 @@ const { Nuxt } = require('nuxt')
 
 const config = {
   dev: false,
-  debug: true,
   buildDir: 'nuxt',
   build: {
     publicPath: '/assets/'
@@ -12,13 +11,10 @@ const config = {
 }
 const nuxt = new Nuxt(config)
 
-function handleRequest(req, res) {
+async function handleRequest(req, res) {
   res.set('Cache-Control', 'public, max-age=600, s-maxage=1200')
-  return new Promise((resolve, reject) => {
-    nuxt.render(req, res, promise => {
-      promise.then(resolve).catch(reject)
-    })
-  })
+  await nuxt.ready()
+  return nuxt.render(req, res)
 }
 
 const app = express()
